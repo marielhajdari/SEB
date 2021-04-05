fd7c35aae189f3a2fe8f6974e8a5eb664e256661
MarielHajdari <hajdarimariel8@gmail.com>
2021-04-05 17:56:19 +0200

    db sql added

---
+++ db_seb.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS tournament CASCADE;
+DROP TABLE IF EXISTS history CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+
+CREATE TABLE users (
+    username text PRIMARY KEY,
+    pwd text NOT NULL,
+    name text,
+    elo integer NOT NULL DEFAULT 100,
+    logged boolean NOT NULL DEFAULT false,
+    participating boolean NOT NULL DEFAULT false
+);
+
+CREATE TABLE history(
+    entryid integer NOT NULL,
+    countPushUps integer, 
+    duration time,
+    usr_name text REFERENCES users(username)
+);
+
+
+CREATE TABLE tournament(
+    id integer NOT NULL,
+    participantName text REFERENCES users(username),
+    totalPushUps integer,
+    isActive boolean NOT NULL DEFAULT true
+);
